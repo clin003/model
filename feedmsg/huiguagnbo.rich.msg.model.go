b45e4079aceed798b90005472982207d0e00a66d
白菜林 <noreply@3ae.cn>
2023-02-09 16:19:47 +0800

    debug

--- feedmsg/huiguagnbo.rich.msg.model.go
+++ feedmsg/huiguagnbo.rich.msg.model.go
@@ -1,46 +1,51 @@
 package feedmsg
 
 import (
-	// "encoding/json"
 	"fmt"
-	// "strings"
-	// "sync"
-	// "time"
-	// "gitee.com/lyhuilin/open_api/model/kvdb"
-	// "gitee.com/lyhuilin/open_api/pkg/errno"
-	// "gitee.com/lyhuilin/open_api/pkg/wsserver"
-	// "gitee.com/lyhuilin/util"
 )
 
 type FeedRichMsgModel struct {
-	Msgtype string                `json:"msgtype"  form:"msgtype"` //rich text image
+	Msgtype string                `json:"msgtype"  form:"msgtype"` //rich text image video
 	MsgID   string                `json:"msgID"  form:"msgID"`
 	MsgTime string                `json:"msgTime"  form:"msgTime"`
 	Text    FeedRichMsgTextModel  `json:"text"  form:"text"`
 	Image   FeedRichMsgImageModel `json:"image"  form:"image"`
+	Video   FeedRichMsgVideoModel `json:"video"  form:"video"`
 	Link    string                `json:"link"  form:"link"`
+	// (Optional)
+	FeedChatId int64 `json:"feed_chat_id,omitempty"`
 }
 type FeedRichMsgTextModel struct {
-	Content      string `json:"content"  form:"content"`
-	ContentEx    string `json:"contentEx"  form:"contentEx"`
-	ContentExPic string `json:"contentExPic"  form:"contentExPic"`
+	Content         string `json:"content"  form:"content"`
+	ContentEx       string `json:"contentEx"  form:"contentEx"`
+	ContentExPic    string `json:"contentExPic"  form:"contentExPic"`
+	ContentMarkdown string `json:"contentMarkdown"  form:"contentMarkdown"`
 }
 type FeedRichMsgImageModel struct {
 	PicURL   string `json:"picURL"  form:"picURL"`
 	FilePath string `json:"filePath"  form:"filePath"`
+	// (Optional)
+	Caption string `json:"caption,omitempty"`
+}
+type FeedRichMsgVideoModel struct {
+	FileURL  string `json:"fileURL"  form:"fileURL"`
+	FilePath string `json:"filePath"  form:"filePath"`
+	// (Optional)
+	Caption string `json:"caption,omitempty"`
 }
 
 func (msg *FeedRichMsgModel) ToString() (res string) {
 	res = fmt.Sprintf("msgID:%s,msgType:%s,msgTime:%s\n", msg.MsgID, msg.Msgtype, msg.MsgTime)
 	if len(msg.Text.Content) > 0 {
-		res = fmt.Sprintf("%s%s\n", res, msg.Text.Content)
+		res = fmt.Sprintf("%s\n%s", res, msg.Text.Content)
 	}
 	if len(msg.Image.PicURL) > 0 {
-		res = fmt.Sprintf("%s%s", res, msg.Image.PicURL)
+		res = fmt.Sprintf("%s\n%s", res, msg.Image.PicURL)
 	}
-	// if len(msg.Link) > 0 {
-	// 	res = fmt.Sprintf("%s\n%s", res, msg.Link)
-	// }
+	if len(msg.Video.FileURL) > 0 {
+		res = fmt.Sprintf("%s\n%s", res, msg.Video.FileURL)
+	}
+
 	return
 }
 
@@ -73,6 +78,33 @@ func NewFeedRichMsgV2(msgType, msgID, msgTime, textContent, textContentEx, textC
 		Image: FeedRichMsgImageModel{
 			PicURL:   imagePicURL,
 			FilePath: imageFilePath,
+		},
+		Link: link,
+	}
+}
+
+func NewFeedRichMsgV3(msgType, msgID, msgTime, textContent, textContentEx, textContentExPic, textContentMarkdown, imagePicURL, imageFilePath, imageCaption, videoFileURL, videoFilePath, videoCaption, link string) FeedRichMsgModel {
+	return FeedRichMsgModel{
+		Msgtype: msgType,
+		MsgID:   msgID,
+		MsgTime: msgTime,
+		Text: FeedRichMsgTextModel{
+			Content:         textContent,
+			ContentEx:       textContentEx,
+			ContentExPic:    textContentExPic,
+			ContentMarkdown: textContentMarkdown,
+		},
+		Image: FeedRichMsgImageModel{
+			PicURL:   imagePicURL,
+			FilePath: imageFilePath,
+			// (Optional)
+			Caption: imageCaption,
+		},
+		Video: FeedRichMsgVideoModel{
+			FileURL:  videoFileURL,
+			FilePath: videoFilePath,
+			// (Optional)
+			Caption: videoCaption,
 		},
 		Link: link,
 	}
